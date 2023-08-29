10fb817eec5852e0bd57cf42de9c04bccd0cc6d2
varathan <varathan@gmail.com>
2023-08-29 14:45:27 +0530

    for ng if and ng loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Event, NavigationEnd, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
+  
   title = 'loginPage';
+
+  loadingornot:boolean=false;
+
+  constructor(private routereve: Router){}
+
+  users = [{name:'varathan',age:20,gender:'m',city:'chennai'},{name:'arjun',age:21,gender:'m',city:'erode'}
+           ,{name:'raju',age:22,gender:'m',city:'erode'},
+          {name:'kala',age:22,gender:'f',city:'erode'}];
+
+  // ngOnInit(): void {
+  //   this.routereve.events.subscribe((routerEven:Event)=>{
+  //      if(routerEven instanceof NavigationStart){
+  //       this.loadingornot=true;
+  //      }
+  //     //  if(routerEven instanceof NavigationEnd){
+  //     //   this.loadingornot=false;
+  //     //  }
+
+  //     setTimeout(()=>{this.loadingornot=false;},1000);
+  //   }
+  //   );
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
